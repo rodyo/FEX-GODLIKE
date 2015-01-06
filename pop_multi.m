de44f68ebf2e91291640c99a1079436bd84ff6cd
Cengiz Gunay <cengique@users.sf.net>
2015-01-06 18:00:57 -0500

    added overload function to initialize num_objectives

--- pop_multi.m
+++ pop_multi.m
@@ -425,7 +425,27 @@ classdef pop_multi < pop_single
                                                             
             end % switch            
         end % method (update_algorithms)
-        
+
+        % overload from pop_single
+        function evaluate_function(pop)
+
+            % ugly hack because of pop_single constructor running
+            % evaluations before pop_multi construction is complete
+            if isempty(pop.num_objectives)
+              num_objectives = pop.options.num_objectives;
+            else
+              num_objectives = pop.num_objectives;
+            end
+          
+            if isempty(pop.pop_data.function_values_offspring)
+              pop.pop_data.function_values_offspring = ...
+                  NaN(pop.size, num_objectives);
+            end
+
+            % call super
+            evaluate_function@pop_single(pop);
+        end
+
     end % protected/hidden methods 
     
 end % classdef
